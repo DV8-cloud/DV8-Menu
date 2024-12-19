dff1b0475f199fe3dd4da5a99176a984d7c2ffd0
DV8-cloud <hazemelsery2@outlook.com>
2024-12-19 12:34:38 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,36 @@
 const pages = document.querySelectorAll('.page');
+const leftArrow = document.querySelector('.left-arrow');
+const rightArrow = document.querySelector('.right-arrow');
+let currentPage = 0;
 
-pages.forEach((page, index) => {
-    page.addEventListener('click', () => {
-        page.style.transform = `rotateY(-180deg)`;
-        page.style.zIndex = 5 + index;
+// عرض الصفحة الحالية
+function showPage(index) {
+    pages.forEach((page, i) => {
+        if (i === index) {
+            page.style.transform = 'rotateY(0deg)';
+            page.style.zIndex = 5;
+            page.classList.remove('hidden');
+        } else {
+            page.classList.add('hidden');
+        }
     });
+}
+
+// التنقل بالسهم الأيمن
+rightArrow.addEventListener('click', () => {
+    if (currentPage < pages.length - 1) {
+        currentPage++;
+        showPage(currentPage);
+    }
 });
+
+// التنقل بالسهم الأيسر
+leftArrow.addEventListener('click', () => {
+    if (currentPage > 0) {
+        currentPage--;
+        showPage(currentPage);
+    }
+});
+
+// عرض الصفحة الأولى عند التحميل
+showPage(currentPage);
